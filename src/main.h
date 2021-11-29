dd7b74cff570b27e93ef96558914a45362c1a324
k-laughner <74153467+k-laughner@users.noreply.github.com>
2021-11-29 02:38:20 -0500

    Create main.h

---
+++ src/main.h
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Kaylee Laughner
+ */
+#include <iostream>
+#include <cmath>
+
+// prototypes
+void printRealResult(double root1, double root2);
+void printImaginaryResult(double root1f, double root1b, double root2f, double root2b);
+void posDeterminantCalc(double a, double b, double c);
+void negDeterminantCalc(double a, double b, double c);
+void zeroDeterminantCalc(double a, double b, double c);
+void calculate(double a, double b, double c);
+void checkRoots(double a, double b, double c, double root);
